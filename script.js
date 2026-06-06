a7efe7fe8e1ff9faf9ef44ed9a8d6eb9692d1122
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2026-06-06 10:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const output = document.getElementById("output");
+function createPromise(){
+	const time = (Math.random()*2 + 1);
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(time);
+		},time*1000);
+	});
+}
+const promise1 = createPromise();
+const promise2 = createPromise();
+const promise3 = createPromise();
+
+const start = performance.now();
+Promise.all([promise1, promise2, promise3]).then((results)=>{
+	const end = performance.now();
+	const total = ((end -start)/1000).toFixed(3);
+	output.innerHTML ="";
+	results.forEach((time,index)=>{
+		output.innerHTML += `
+		<tr>
+			<td>Promise ${index +1}</td>
+			<td>${time.toFixed(3)}</td>
+		</tr>`; 
+	});
+	output.innerHTML += `
+		<tr>
+			<td>Total</td>
+			<td>${total}</td>
+		</tr>
+	`;
+});
